00fa0748143442aca27ea41ef1d6241f8affd680
JT4848 <jtpricone@gmail.com>
2023-12-22 09:26:49 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import { useState } from 'react'
 import './App.css'
 
+
 const App = () => {
+const [listItem, setListItem] = useState('');
+const [toDo, setToDo] = useState([]);
+
+const handleInputChange = (event) => {
+  setListItem(event.target.value);
+}
+
+const handleAdd = (event) => {
+  if(listItem.trim() !== ''){
+    setToDo((prevlist) => [...prevlist, listItem]);
+    setListItem('');
+    event.preventDefault();
+  }
+}
+
+const handleDelete = (index) => {
+  setToDo((prevlist) => prevlist.filter((_, i) => i !== index));
+}
+
 
   return (
     <>
+    <form onSubmit={(event) => handleAdd(event)}>
+      <label>To do: </label>
+        <input placeholder='type here' value={listItem} onChange={handleInputChange}/>
+      <button>Add</button>
+    </form>
+
+    {toDo.map((list, index) => {
+        return (
+          <>
+          <h3 key={index}>{list}</h3>
+          <button onClick={() => handleDelete(index)}>Delete</button>
+          </>
+        );
+      })}
     </>
   )
 }
